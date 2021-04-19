ff57129ba4c09ffbc49676fe17394b5b86fdbb23
j.ventura1738 <justinv9001@aol.com>
2021-04-19 13:04:43 -0400

    optimized prime

--- gullcode.py
+++ gullcode.py
@@ -15,11 +15,13 @@ def is_prime(n: int) -> bool:
     # Comment out if program gets too slow
     assert(type(n) == int), 'Argument must be integer.'
 
-    if n % 2 is 0:
+    if n is 2:
+        return True
+    elif n % 2 is 0 or n is 1:
         return False
 
     for i in range(2, math.ceil(math.sqrt(n)) + 1):
-        if n % 1 is 0:
+        if n % i is 0:
             return False
 
     return True
@@ -28,23 +30,16 @@ def is_prime(n: int) -> bool:
 def nth_prime(n: int) -> int:
     # Comment out if program gets too slow
     assert (type(n) == int), 'Argument must be integer.'
-    if n == 1:
-        return 2
-    elif n == 2:
-        return 3
-    elif n == 3:
-        return 5
 
-    curr = 3
-    val = 7
-    while curr <= n:
-        if is_prime(val):
-            curr += 1
-        if curr == n:
-            return val
-        else:
-            val += 2
+    p = 1
+    i = 1
+    while True:
+        if is_prime(i):
+            p += 1
+        if p == n:
+            return i
+        i += 2
 
 
 if __name__ == '__main__':
-    is_prime(10001)
+    print(nth_prime(6969))
